8bcf0e9366084da606ae34f66f6146f25827cda6
Philipp R <philipp.remplbauer@gmx.at>
2016-11-25 11:30:40 -0500

    Added some frameworks and created login and register menu, currently without backend of this fields

--- Web/index.php
+++ Web/index.php
@@ -6,17 +6,24 @@
     <!--CSS Sheets -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/global.css">
+    <link rel="stylesheet" href="./css/swal-forms.css">
+    <link rel="stylesheet" type="text/css" href="./css/sweetalert.css">
+
 
     <title>Todolist - TaskManager</title>
   </head>
   <body>
     <div id="mainButtons">
-      <button type="submit" class="cButton btn btn-default" onclick="onRegisterClick()">Register</button>
-      <button type="submit" class="cButton btn btn-default" onclick="onLoginClick()">Login</button>
-    </div
+      <button class="cButton btn btn-default" onclick="onRegisterClick()">Register</button>
+      <button class="cButton btn btn-default" onclick="onLoginClick()">Login</button>
+    </div>
 
     <!-- Scripts -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-    <script src="./js/global.js"/>
+    <script src="./js/global.js"></script>
+    <script src="./js/sweetalert.min.js"></script>
+    <script src="./js/swal-forms.js"></script>
+
   </body>
 </html>
--- Web/js/global.js
+++ Web/js/global.js
@@ -1,9 +1,35 @@
 function onRegisterClick()
 {
-
+  swal.withForm({
+    title: 'Register an account',
+    text: 'Type in your username and password!',
+    showCancelButton: true,
+    confirmButtonColor: '#2ecc71',
+    confirmButtonText: 'Register',
+    closeOnConfirm: true,
+    formFields: [
+        { id: 'username', placeholder:'username' },
+        { id: 'password', placeholder:'password' }
+    ]
+  }, function(isConfirm) {
+      console.log(this.swalForm) // json object of input fields!
+  });
 }
 
 function onLoginClick()
 {
-  
+  swal.withForm({
+    title: 'Login to your account',
+    text: 'Type in your username and password!',
+    showCancelButton: true,
+    confirmButtonColor: '#2ecc71',
+    confirmButtonText: 'Login',
+    closeOnConfirm: true,
+    formFields: [
+        { id: 'username', placeholder:'username' },
+        { id: 'password', placeholder:'password' }
+    ]
+  }, function(isConfirm) {
+      console.log(this.swalForm) // json object of input fields!
+  });
 }
