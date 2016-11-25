8e6cb5643ee962347357b482ef49d63f710f2d06
Philipp R <philipp.remplbauer@gmx.at>
2016-11-25 09:13:58 -0500

    simple setup of the controller

---
+++ TaskManager/src/net/teammagic/taskmanager/Controller.java
@@ -0,0 +1,14 @@
+package net.teammagic.taskmanager;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable{
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("<init>");
+    }
+}
